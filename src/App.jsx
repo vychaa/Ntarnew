1849ff3b7aae512cf28cb97ac459bffc36dd6f3a
ram <ramakrishna1042000@gmail.com>
2024-08-17 22:50:13 +0530

    ntar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Router } from "react-router-dom"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <BrowserRouter>
+      <Router>
+        <Route path="/" element={} />
+      </Router>
+     </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Import Bootstrap JS bundle
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
