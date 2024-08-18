a57a9e9799e2c63d0a4ce836f5252ea51dd54bc3
unknown <vychaa671@gmail.com>
2024-08-18 13:00:42 +0530

    ui changes

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,9 +1,9 @@
 // Carousel.js
-import React from 'react';
-import Slider from 'react-slick';
-import './Carousel.css'; // Ensure this file has the appropriate styles
-import carousel1 from '../../assets/images/Background.webp';
-import carousel2 from '../../assets/images/background2.webp';
+import React from "react";
+import Slider from "react-slick";
+import "./Carousel.css"; // Ensure this file has the appropriate styles
+import carousel1 from "../../assets/images/Background.webp";
+import carousel2 from "../../assets/images/background2.webp";
 
 const SampleNextArrow = (props) => {
   const { onClick } = props;
@@ -31,11 +31,35 @@ const Carousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 7000, // Slide speed
+    autoplaySpeed: 2000, // Slide speed
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
-
+  const isMobile = window.innerWidth <= 768;
+  if (!isMobile) {
+    return (
+      <div className="carousel-container web-Carousel">
+        <div className="slide">
+          <img src={carousel1} alt="Slide 1" />
+          <div className="text-overlay">
+            <p className="title_crl">SPEND</p>
+            <p className="card-sub-crl-text">
+              Pay all your<br></br>Bills in one<br></br>easy app
+            </p>
+          </div>
+        </div>
+        <div className="slide">
+          <img src={carousel2} alt="Slide 2" />
+          <div className="text-overlay">Another Slide</div>
+        </div>
+        <div className="slide">
+          <img src={carousel1} alt="Slide 3" />
+          <div className="text-overlay">Yet Another Slide</div>
+        </div>
+        {/* Add more slides as needed */}
+      </div>
+    );
+  }
   return (
     <div className="carousel-container">
       <Slider {...settings}>
