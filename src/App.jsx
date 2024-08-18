8831387a776695d5d5ca5c56d5325998c13f6e05
unknown <vychaa671@gmail.com>
2024-08-18 23:58:25 +0530

    my app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
-import  './app.css'
+// import  './app.css'
 
 function App() {
 
